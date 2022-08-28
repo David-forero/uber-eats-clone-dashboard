2b4df14858b95977b061b16977c1fee7a144e839
David Forero <54685136+David-forero@users.noreply.github.com>
2022-08-28 12:03:19 -0400

    create sibe menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import dishes from './assets/data/dishes.json';
 import DetailedOrder from './modules/DetailedOrder';
+import Orders from './modules/Orders';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout, Image } from 'antd';
+import SideMenu from '../src/components/SideMenu';
+
+const { Sider, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div>
-      <DetailedOrder/>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Sider style={{ height: "100vh", backgroundColor: 'white' }}>
+          <Image
+            src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg"
+            preview={false}
+          />
+
+          <SideMenu/>
+        </Sider>
+        <Layout>
+          <Content>
+            <Routes>
+              <Route path='' element={<Orders />} />
+              <Route path='order/:id' element={<DetailedOrder />} />
+            </Routes>
+          </Content>
+
+          <Footer style={{ textAlign: "center" }}>
+            Uber Eats Restaurant Dashboard Clone
+          </Footer>
+        </Layout>
+      </Layout>
+
+    </BrowserRouter>
   );
 }
 
